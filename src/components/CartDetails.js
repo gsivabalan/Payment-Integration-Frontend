29713de4a40726c612d948e6e90952c1be61be07
gsivabalan <siva90.balan@gmail.com>
2023-11-29 10:58:47 +0530

    second commit

--- src/components/CartDetails.js
+++ src/components/CartDetails.js
@@ -69,7 +69,7 @@ const CartDetails = () => {
     const headers = {
       "Content-Type": "application/json"
     };
-    const response = await fetch("http://localhost:7000/api/create-checkout-session", {
+    const response = await fetch("https://vercel.com/gsivabalan/payment-integration-backend/2dRu91kBvscA93eEgWKtMAyAPm8L", {
       method: "POST",
       headers: headers,
       body: JSON.stringify(body)
