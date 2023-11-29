75da55d9d848eb9c58cbf600b54e0f7ac5db9070
gsivabalan <siva90.balan@gmail.com>
2023-11-29 09:54:48 +0530

    first commit

---
+++ src/components/CardData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        dish: "chettinad biryani",
+        imgdata: "http://2.bp.blogspot.com/-LlJPPCUIF7I/TuyIRGlHI4I/AAAAAAAANh0/kGiQWzvHpS4/s1600/cb+yes.jpg",
+        address: "South Indian, Biryani",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed from here recently",
+        price: 350,
+        rating: "3.8",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        dish: "Mutton Cury with Rice",
+        imgdata: "https://i.ytimg.com/vi/6EsLhGCYnEg/maxresdefault.jpg",
+        address: "Street Food",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 400,
+        rating: "3.9",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        dish: "La Milano Pizza",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/19708611/10f90d4a69678d98662514d173b29665_o2_featured_v2.jpg",
+        address: "Pizza, Fast Food, Pasta",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 70,
+        rating: "4.2",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        dish: "Momoman Momos",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/1/113401/59f29399060caefcc575d59dc9402ce8_o2_featured_v2.jpg",
+        address: "Momos",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 70,
+        rating: "3.8",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        dish: "Paneer Butter Masala + Rotti",
+        imgdata: "https://www.awesomecuisine.com/wp-content/uploads/2008/02/paneer_butter_masala_with_roti.jpg",
+        address: "North Indian",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 210,
+        rating: "4.0",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        dish: "Spring Rolls",
+        imgdata: "https://b.zmtcdn.com/data/pictures/5/113895/3c06f6fbb8f667a2537dfdb6f060dc8b_o2_featured_v2.jpg",
+        address: "Wraps FastFood, Chines",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 100,
+        rating: "3.8",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        dish: "Aalu Poori + Chana Cury",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/5/110155/811c01a5430d50d3260f77917da99e12_o2_featured_v2.jpg",
+        address: "North Indian, Fast Food",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 180,
+        rating: "3.8",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        dish: "Mutton Kabab",
+        imgdata: "http://recipes.timesofindia.com/photo/58360750.cms",
+        address: "Tea, Coffee, Shake, Beverages",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 280,
+        rating: "3.2",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        dish: "Fast Food",
+        imgdata: "https://b.zmtcdn.com/data/pictures/7/19639627/94c0a4cf15c02d3982d154e2c5dd8cbb_o2_featured_v2.jpg",
+        address: "Burger, Sandwich, Fast Food",
+        //delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 100,
+        rating: "3.8",
+        //arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
